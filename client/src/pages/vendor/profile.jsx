757cdcd2545a24ba0193fe6ef764383836d45b92
Akif Rahman <akifrahman90442@gmail.com>
2023-01-23 21:32:12 +0530

    edit commit

--- client/src/pages/vendor/profile.jsx
+++ client/src/pages/vendor/profile.jsx
@@ -11,6 +11,9 @@ import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual';
 import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -68,9 +71,38 @@ export default function Profile() {
                     <Grid xs={6}>
                         <IconButton size='small' sx={{color:'#111' , borderRadius:'10px'}}>
                           <SmartDisplayIcon/>
-                          <h4 style={{marginLeft:'6px' , fontFamily:'sans-serif' }}>Photos</h4>
+                          <h4 style={{marginLeft:'6px' , fontFamily:'sans-serif' }}>Vidoes</h4>
                       </IconButton>
                     </Grid>
+                  </Grid>
+                  <Grid xs={12} sx={{ mt: 2 }}>
+                      <Box sx={{ display:'flex' }}>
+                          <img src="/null-profile.jpg" style={{ width:'8%' , height:'fit-content' ,borderRadius:'50%',border:'1px solid #000' }} alt="" />
+                          <h5 style={{ fontWeight:'900' , margin:'5px'}}>Akif Rahman</h5>
+                      </Box>
+                      <p style={{ marginTop:'15px' , fontSize:'13px'}}>Its New Laptop</p>
+                      <img src="https://i.pcmag.com/imagery/reviews/065rv6nxdAEcCzvE3Qb8T3v-1.fit_lim.size_840x473.v1658424542.jpg" style={{ marginTop:'10px' , width:'100%' , height:'fit-content' ,borderRadius:'5px',border:'1px solid #000' }} alt="" />
+                      <Box sx={{ borderRadius:'5px' , backgroundColor:'#f5f5f5' , textAlign:'center' , display:'flex' }}>
+                          <Grid xs={4}>
+                            <IconButton size='large' sx={{color:'black'}}>
+                                <ThumbUpOffAltIcon/>
+                            </IconButton>
+                          </Grid>
+                          <Grid xs={4}>
+                            <IconButton size='large' sx={{color:'black'}}>
+                                <QuestionAnswerOutlinedIcon/>
+                            </IconButton>
+                          </Grid>
+                          <Grid xs={4}>
+                            <IconButton size='large' sx={{color:'black'}}>
+                                <ShareOutlinedIcon/>
+                            </IconButton>
+                          </Grid>
+                      </Box>
+                      <Box sx={{ mt: 2 , fontFamily:'sans-serif' , display:'flex'}}>
+                          <p><b>150</b> Like </p><img src="/null-profile.jpg" style={{ marginLeft:'5px' , width:'18px' , height:'fit-content' ,borderRadius:'50%',border:'1px solid #000' }} alt="" /><h6 style={{ margin:'4px' }}>Minhaj and Others</h6>
+                      </Box>
+                      
                   </Grid>
                 </Box>
                 </Grid>
